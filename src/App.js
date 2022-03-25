9e9e4668bd2a93d03209014e284116500ea45e40
wiusxt <ashatovanas65@gmail.com>
2022-03-25 12:53:44 +0600

    Paragraph.js file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Paragraph from "./components/Paragraph/Paragraph";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      </div>
+      <Paragraph size="16px" color="#000000">this is a sample paragraph</Paragraph>
 
+</div>
   );
 }
 
---
+++ src/components/Paragraph/Paragraph.js
@@ -0,0 +1,9 @@
+function Paragraph(props) {
+    const style = {
+        fontSize: props.size
+    };
+
+    return <p style={style}>{props.children}</p>;
+}
+
+export default Paragraph;
